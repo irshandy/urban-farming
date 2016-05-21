be9c19d723487c94c332bda495ba39e750ddbc76
Irvin Shandy <irvin.shandy@gmail.com>
2016-05-21 14:29:59 +0700

    Update code for Android compatibilities

--- SmartUrbanFarming.ino
+++ SmartUrbanFarming.ino
@@ -8,6 +8,7 @@
 #define soilSensorClockPin  7
 #define airSensorPin        4
 #define airSensorType       DHT11
+#define numLED              30
 
 // Plant types
 #define babyLeafLettuce 0
@@ -21,10 +22,19 @@ float soilDewpoint;
 float airHumidity;
 float airTemp;
 
+// Variables for LED
+long timePassed = 0;
+long timeStart = 0;
+byte lastLedStatus = 0;
+
+// Variables for Android
+String intData = "";
+char intBuffer[12];
+
 // Create sensor and LED instance
 Sensirion soilSensor = Sensirion(soilSensorDataPin, soilSensorClockPin);
 DHT airSensor = DHT(airSensorPin, airSensorType);
-Adafruit_NeoPixel led = Adafruit_NeoPixel(60, ledPin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led = Adafruit_NeoPixel(numLED, ledPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
@@ -44,12 +54,46 @@ void loop() {
       readSensor();
       sendAndroidValues();
     }
+    else if ( c == 'i') {
+      intData.toCharArray(intBuffer, intData.length() + 1);
+      int wavelength = atoi(intBuffer);
+      Serial.println(wavelength);
+      if (lastLedStatus == 0) {
+        if (wavelength == 660) {
+          setLEDColor(cabbage);
+        } else if (wavelength == 530) {
+          setLEDColor(babyLeafLettuce);
+        } else if (wavelength == 661) {
+          setLEDColor(basil);
+        }
+        lastLedStatus = 1;
+      }
+      intData = "";
+    }
   }
 
-  colorWipe(basil);
+  setLEDColor(basil);
+  switchLED();
   
   // Wait 100 ms before next measurement
   delay(100);  
+}
+
+void switchLED() {
+  timePassed += millis() - timeStart;
+  if (timePassed >= 60 * 60 * 8) {
+    timePassed = 0;
+    if (lastLedStatus == 0) {
+      led.setBrightness(0);
+      led.show();
+      lastLedStatus = 1;
+    }
+    else {
+      led.setBrightness(255);
+      led.show();
+      lastLedStatus = 0;
+    }
+  }
 }
 
 void readSensor() {
@@ -59,12 +103,12 @@ void readSensor() {
 }
 
 void printData() {
-  //Serial.print("Soil Humidity: ");
-  //Serial.print(soilHumidity);
-  //Serial.print("%\t");
-  //Serial.print("Soil Temperature: ");
-  //Serial.print(soilTemp);
-  //Serial.println("*C");
+  Serial.print("Soil Humidity: ");
+  Serial.print(soilHumidity);
+  Serial.print("%\t");
+  Serial.print("Soil Temperature: ");
+  Serial.print(soilTemp);
+  Serial.println("*C");
   Serial.print("Air Humidity: ");
   Serial.print(airHumidity);
   Serial.print("%\t");
@@ -76,15 +120,20 @@ void printData() {
 void sendAndroidValues() {
   Serial.print('#');
   Serial.print(airHumidity);
+  Serial.print('#');
   Serial.print(airTemp);
+  Serial.print('#');
   Serial.print(soilHumidity);
+  Serial.print('#');
   Serial.print(soilTemp);
+  Serial.print('#');
+  Serial.print(lastLedStatus);
   Serial.print('~');
   Serial.println();
   delay(10);
 }
 
-void colorWipe(int plantType) {
+void setLEDColor(int plantType) {
   switch (plantType) {
     case 0:
       for(int i=0; i<led.numPixels(); i++) {
